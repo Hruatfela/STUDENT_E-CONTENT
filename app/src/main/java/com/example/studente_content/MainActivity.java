3fb0d1daf53ad82c0c7c52533873d8beb49650f4
ha <linxvenom15@gmail.com>
2023-03-10 13:58:06 +0530

    3rd commit

--- app/src/main/java/com/example/studente_content/MainActivity.java
+++ app/src/main/java/com/example/studente_content/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.studente_content;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStd, btnAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStd = findViewById(R.id.std_ID);
+        btnAdmin = findViewById(R.id.adminID);
+
+        btnStd.setOnClickListener(view -> {
+            startActivity(new Intent(this,StudentActivity.class));
+        });
     }
 }
